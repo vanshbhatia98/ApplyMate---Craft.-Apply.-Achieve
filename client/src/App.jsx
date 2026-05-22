1ddd3f33215ba840d0d36e6824f1e9c1def8685c
Vansh Bhatia <bhatiavansh49@gmail.com>
2026-05-22 09:43:06 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import ResumeBuilder from './pages/ResumeBuilder'
+import Preview from './pages/Preview'
+import Login from './pages/Login'
+import { useDispatch } from 'react-redux'
+import api from './configs/api'
+import { login, setLoading } from './app/features/authSlice'
+import {Toaster} from 'react-hot-toast'
+
+const App = () => {
+
+  const dispatch = useDispatch()
+
+  const getUserData = async() => {
+    const token = localStorage.getItem('token');
+    try {
+      if(token){
+        const { data } = await api.get('/api/users/data', {headers: {Authorization: token}})
+        if(data.user){
+          dispatch(login({token, user: data.user}))
+        }
+        dispatch(setLoading(false))
+      } else {
+        dispatch(setLoading(false))
+      }
+    } catch (error) {
+      dispatch(setLoading(false))
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getUserData()
+  }, [])
+
+  return (
+    <>
+    <Toaster />
+       <Routes>
+         <Route path='/' element={<Home />}/>
+         <Route path='app' element = {<Layout />}>
+           <Route index element = {<Dashboard />}/>
+           <Route path='builder/:resumeId' element = {<ResumeBuilder/>} />
+         </Route>
+
+         <Route path='view/:resumeId' element={<Preview />}/>
+         {/* I want to open the login page whenever the use is not logged in and whenver user tried to open the Dashboard or app  */}
+         {/* <Route path='login' element={<Login />}/> */} 
+       </Routes>
+    </>
+  )
+}
+
+export default App
